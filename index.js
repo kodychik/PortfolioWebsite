d157bcf665525b4f00698d968614aedf2f072199
kodychik <73432901+kodychik@users.noreply.github.com>
2023-07-14 14:11:23 +0800

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+//my first js file
+
+// What I have covered with bro code
+
+// user input kind of thing
+/*
+let username = window.prompt("what's your name?");
+document.getElementById("output").innerText = "Hello, " + username + "!";
+*/
+// type conversion
+
